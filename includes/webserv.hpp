98e4455c0cb96628b4e2f113275d54d1da47100e
Alexis Bidolet--Foray <abidolet@z3r9p2.42lyon.fr>
2025-06-02 16:14:35 +0200

    struct

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -1,5 +1,28 @@
 #ifndef WEBSERV
 #define WEBSERV
 
-#endif // !WEBSERV
-#define WEBSERV
+#include <iostream>
+#include <vector>
+#include <map>
+
+struct Location
+{
+	std::string					path;
+	std::string					root;
+	std::string					redirect;
+	std::string					cgi_pass;
+	std::vector<std::string>	cgi_extensions;
+};
+
+struct Server
+{
+	int							port;
+	std::string					host;
+	std::string					server_name;
+	bool						is_default;
+	std::map<int, std::string>	error_pages;
+	size_t						client_max_body_size;
+	std::vector<Location>		locations;
+};
+
+#endif
