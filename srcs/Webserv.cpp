9ad56cc0b8eb80d017afa3989db8bad93ad53d97
Alexis Bidolet--Foray <abidolet@z3r9p2.42lyon.fr>
2025-06-03 08:27:34 +0200

    tabs

--- srcs/Webserv.cpp
+++ srcs/Webserv.cpp
@@ -1,21 +1,21 @@
 #include "Webserv.hpp"
 
 Webserv::Webserv()
-    : servers()
+	: servers()
 {
-    Server  default_server;
+	Server  default_server;
 
-    default_server.port = 8080;
-    default_server.host = "0.0.0.0";
-    default_server.server_name = "nginx";
-    default_server.is_default = true;
-    servers.push_back(default_server);
+	default_server.port = 8080;
+	default_server.host = "0.0.0.0";
+	default_server.server_name = "nginx";
+	default_server.is_default = true;
+	servers.push_back(default_server);
 }
 
 Webserv::Webserv(const std::string& file)
-    : servers()
+	: servers()
 {
-    (void)file;
+	(void)file;
 }
 
 Webserv::~Webserv()
