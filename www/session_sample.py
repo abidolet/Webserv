171c094287f48475030a017b61a720bae8cbcff3
Maxime Juncker <maxime.junck@gmail.com>
2025-06-19 09:26:41 +0200

    session api

---
+++ www/session_sample.py
@@ -0,0 +1,21 @@
+import sys
+import requests
+
+body = {
+	"grant_type" : "client_credentials",
+}
+
+url = "http://127.0.0.1:8008"
+response = requests.post(url, data=body)
+if (response.status_code != 200):
+	sys.exit()
+
+uid = response.text
+print(f"client uid has been aquired, uid:{uid}")
+body = {
+	"grant_type" : "client_visits",
+	"UID": str(uid),
+}
+response = requests.post(url, data=body)
+if (response.status_code == 200):
+	print(f"you have made {response.text} request in total")
