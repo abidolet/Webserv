3d447e2125aef46297e9bcb65dfd7697ffc2a1b4
Maxime Juncker <maxime.junck@gmail.com>
2025-06-20 15:34:21 +0200

    fix?

--- test/tester.sh
+++ test/tester.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-exec ../webserv ../conf/action.conf &
-exec siege -c 2 -r 100 -b -t 1s --delay=0 http://127.0.0.1:8080 > log.txt
+../webserv ../conf/action.conf &
+siege -c 2 -r 100 -b -t 1s --delay=0 http://127.0.0.1:8080 > log.txt
 
 sleep 5
 cat log.txt
