ebd642fa076648aeba9edff411fbb4677ee32ad0
Maxime Juncker <maxime.junck@gmail.com>
2025-06-20 15:35:31 +0200

    30min tester

--- test/tester.sh
+++ test/tester.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ../webserv ../conf/action.conf &
-siege -c 2 -r 100 -b -t 1s --delay=0 http://127.0.0.1:8080 > log.txt
+siege -c 20 -r 100 -b -t 30m --delay=0 http://127.0.0.1:8080 > log.txt
 
 sleep 5
 cat log.txt
