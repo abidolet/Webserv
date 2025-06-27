3e33e025644ca591ac8677739deac9245a8224f3
Maxime Juncker <maxime.junck@gmail.com>
2025-06-27 11:54:53 +0200

    remove debug logs

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -29,6 +29,7 @@ int main (int argc, char *argv[])
 	signal(SIGQUIT, handle_signal);
 	signal(SIGTERM, handle_signal);
 
+	Log::disableFlags(F_DEBUG);
 	#if ENABLE_LOG_FILE
 		Log::setupLogFile();
 	#endif
@@ -64,7 +65,4 @@ int main (int argc, char *argv[])
 	}
 	return (0);
 }
-
-
-
 
